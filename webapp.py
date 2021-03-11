612db6b17523954aaf19673d646b9771d0afe9c1
Korkz <77691313+Korkz@users.noreply.github.com>
2021-03-11 02:49:39 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,14 +1,27 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
-
-@app.route("/p1")
-def render_page1():
-    return render_template('page1.html')
     
+@app.route("/calculate")
+def render_calculate():
+    xval = request.args['xval']
+    yval = request.args['yval']
+    operation = request.args['operation']
+    
+    if operation == 'add'
+        answer = xval + yval
+    if operation == 'sub'
+        answer = xval - yval 
+    if operation == 'mul'
+        answer = xval * yval 
+    if operation == 'div'
+        answer = xval / yval        
+    
+    return render_template('page1.html', response = answer)
+     
 if __name__=="__main__":
     app.run(debug=False)
